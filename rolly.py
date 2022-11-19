5d309bc59e3d213e3eb2a7e3b3ab88c47213529c
me11203sci <ma6hv@umsystem.edu>
2022-11-18 20:32:41 -0600

    Converted indentations from tabs to spaces. Refactored do() command. Fixed bug where temporary/ folder was not being deleted.

--- rolly.py
+++ rolly.py
@@ -1,4 +1,4 @@
-# Imports
+# Imports.
 from dotenv import load_dotenv
 from discord.ext import commands
 import discord
@@ -14,51 +14,52 @@ rolly = commands.Bot(command_prefix = '!', intents = intents)
 
 @rolly.event
 async def on_ready():
-    notes_queue = rolly.get_channel(1037976833221726268)
-    await notes_queue.send("I am retarded (and ready to help)!")
+	notes_queue = rolly.get_channel(1037976833221726268)
+	await notes_queue.send("I am retarded (and ready to help)!")
 
 @rolly.command()
 async def poke(context):
-    """( ´∀` )σ)`A´ )"""
-    await context.send("Um, you stink and leave me alone please!")
+	"""( ´∀` )σ)`A´ )"""
+	await context.send("Um, you stink and leave me alone please!")
 
 @rolly.command()
 async def clear(context):
-    """Clears channel."""
-    async for message in context.channel.history():
-        await message.delete()
+	"""Clears channel."""
+	async for message in context.channel.history():
+		await message.delete()
 
 @rolly.command()
 async def do(context):
-    """Downloads, Deletes, Converts, Renames, and Relocates files"""
-    os.system("mkdir temporary && cp .env temporary/.env && cp hashVisuals.sh temporary/hashVisuals.sh")
-    print("!DO: Created directory \"temporary\"")
-    i=0
-    async for message in context.channel.history():
-        if(len(message.attachments) == 0):
-            await message.delete()
-        for attachment in message.attachments:
-            if(attachment.content_type != "application/pdf"):
-                await message.delete()
-            else:
-                await attachment.save(fp = f"temporary/{i}.pdf")
-                await message.delete()
-                i+=1
-    previousDirectory = os.getcwd()
-    os.chdir("temporary")
-    for file in os.listdir():
-        if(file.split(".")[1] == "pdf"):
-            print(f"!DO: Converting {file}")
-            outputName = file.split(".")[0]
-            os.system(f"pdfimages -j -p {file} {outputName} && rm {file}")
-    for picture in list(filter(lambda x: x.split(".")[1] == "jpg", os.listdir())):
-        os.system(f"sh hashVisuals.sh {picture}")
-    os.chdir(previousDirectory)
-    os.system("rm -rf temporary")
-    print("!DO: Deleted directory \"temporary\"")
-    print("All done. *burps*")
+	"""Downloads, Converts, Deletes, Renames, and Relocates P.D.F. files"""
+	# Create temporary directory to store downloads.
+	os.system("mkdir temporary")
+	print("!do COMMAND: Created directory \"temporary\"")
+	
+	# Iterate through all messages in channel.
+	file_name = 0
+	directory = "temporary/"
+	async for message in context.channel.history():
+		for attachment in message.attachments:
+			if(attachment.content_type == "application/pdf"):
+				path = directory + str(file_name)
+				await attachment.save(f"{path}.pdf")
+				print(f"!do COMMAND: Downloaded file {file_name}.pdf")
+				os.system(f"pdfimages -j -p {path}.pdf {path}")
+				print(f"!do COMMAND: Converted {file_name}.pdf")
+				os.system(f"rm {path}.pdf")
+				print(f"!do COMMAND: Deleted {file_name}.pdf")
+				for file in os.listdir(directory):
+					os.system(f"sh hashVisuals.sh {directory}{file}")
+				file_name += 1
+		await message.delete()
+	
+	# Wrap-up.
+	os.system("rm -rf temporary/")
+	print("!do COMMAND: Deleted directory \"temporary\"")
+	print("All done. *burps*")
 
-envPath = os.path.join(os.getcwd(),".env")
-load_dotenv(envPath)
+# Retriveing and utilizing access token.
+env_path = os.path.join(os.getcwd(),".env")
+load_dotenv(env_path)
 token = os.getenv("BOT_TOKEN")
 rolly.run(token)
